d3be70bba195052e2cf841bfc061a1c28f064062
rosie <f.werth@gmail.com>
2018-05-26 21:46:02 -0400

    Screen clipping accessibility service working, text fields retrieved successfully.

---
+++ app/src/main/java/com/longlivemydog/rosie/gloss/TextClippingService.java
@@ -0,0 +1,69 @@
+package com.longlivemydog.rosie.gloss;
+
+import android.accessibilityservice.AccessibilityService;
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.view.accessibility.AccessibilityWindowInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextClippingService extends AccessibilityService {
+
+    ArrayList<AccessibilityNodeInfo> mTextNodes;
+
+    public TextClippingService() {
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.v("TCS", "onServiceConnected()");
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.v("TCS", "onAccessibilityEvent()");
+//        AccessibilityNodeInfo nodeInfo = event.getSource();
+//        if(nodeInfo == null)
+//            return;
+
+        mTextNodes = new ArrayList<>();
+
+        List<AccessibilityWindowInfo> windows = getWindows();
+        ArrayList<AccessibilityNodeInfo> nodes = new ArrayList<>();
+        for(AccessibilityWindowInfo window : windows) {
+            nodes.add(window.getRoot());
+        }
+
+        for(int i = 0; i < nodes.size(); i++) {
+            AccessibilityNodeInfo node = nodes.get(i);
+            if(node == null)
+                continue;
+
+            CharSequence text = node.getText();
+            if(text != null) {
+                Log.v("TCS", "--> " + text.toString());
+                Rect bounds = new Rect();
+                node.getBoundsInScreen(bounds);
+                Log.v("TCS", "    " + bounds.toShortString());
+                mTextNodes.add(node);
+            }
+
+            for(int j = 0; j < node.getChildCount(); j++) {
+                nodes.add(node.getChild(j));
+            }
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+        Log.v("TCS", "onInterrupt()");
+    }
+
+}
